bebb2dd7f819ab7861c962de62083775373630b0
bountyhunter01 <ggiback725@gmail.com>
2024-02-01 11:48:51 +0900

    Add files via upload

---
+++ 44-test.js
@@ -0,0 +1,15 @@
+
+            var arrData = []; 
+
+            function fillArray(n) {
+                
+                arrData = [];
+
+               
+                for(var i = 1; i <= n; i++) {
+                    arrData.push(i);
+                }
+            }
+
+          
+            document.write(arrData);
